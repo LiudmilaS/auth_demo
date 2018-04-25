7629355e25c9dd89e1cef7815c78191da1b3f35a
LiudmilaS <liudmila03@gmail.com>
2018-04-25 10:17:42 +0100

    Update settings.py

--- auth_demo/settings.py
+++ auth_demo/settings.py
@@ -79,11 +79,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'auth_demo.wsgi.application'
 
 
-DATABASES = [{'default': {'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': os.path.join(BASE_DIR, 'db.sqlite3')},
-             },
-             {'default': dj_database_url.config('CLEARDB_DATABASE_URL'),
-             }]
+DATABASES = {
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
